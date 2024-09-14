f307ee0c092d3132b3885409fb8161d6422f0972
danmlarsen <larsendanmarius@gmail.com>
2024-09-14 14:33:19 +0200

    Minor adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,14 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                'soft-blue': 'hsl(215, 51%, 70%)',
                 cyan: 'hsl(178, 100%, 50%)',
-                'verydark-blue-900': 'hsl(217, 54%, 11%)',
-                'verydark-blue-700': 'hsl(216, 50%, 16%)',
-                'verydark-blue-500': 'hsl(215, 32%, 27%)',
+                blue: {
+                    900: 'hsl(217, 54%, 11%)',
+                    800: 'hsl(216, 50%, 16%)',
+                    700: 'hsl(215, 32%, 27%)',
+                    500: 'hsl(215, 51%, 70%)',
+                    default: 'hsl(215, 51%, 70%)',
+                },
             },
             fontFamily: {
                 sans: ['Outfit', 'sans-serif'],
